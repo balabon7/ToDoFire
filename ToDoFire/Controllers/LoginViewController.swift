7606ce2de7dd893f6c67466f3e43f0d865f96f18
Alex Balabon <balabon7@gmail.com>
2020-07-23 16:18:06 +0300

    Keyboard issue

--- ToDoFire/Controllers/LoginViewController.swift
+++ ToDoFire/Controllers/LoginViewController.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var warnLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardDidShow(notification:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
     }
-
+    
+    @objc private func keyboardDidShow(notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        
+        let kbFrameSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height + kbFrameSize.height)
+        
+        (self.view as! UIScrollView).scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: kbFrameSize.height, right: 0)
+    }
+    
+    
+    @objc private func keyboardDidHide() {
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+    }
+    
+    
     @IBAction func loginTapped(_ sender: UIButton) {
         
     }
